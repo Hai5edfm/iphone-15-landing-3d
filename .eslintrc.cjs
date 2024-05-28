444009b897918538e15c4045c590cb0706366cf1
hai5edfm <haisedfm@hai5e.com>
2024-05-28 11:43:31 -0600

    add assets, configs and basic components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'standard',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'max-len': [2, 250],
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
